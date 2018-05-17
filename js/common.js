5a8050124598f6b2891843dc09626b294f6711ad
RandyTjang <randy.tjang@gmail.com>
2018-05-18 01:17:00 +1000

    Fixed nav far (finally)

--- js/common.js
+++ js/common.js
@@ -1,22 +1,16 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
-
-$(function(){
+$( document ).ready(function() {
   var includes = $('[data-include]');
   jQuery.each(includes, function(){
     var file = 'views/' + $(this).data('include') + '.html';
     $(this).load(file, function() {
+      // Everything below here is doen in callback for imported content
+      // ie content imported must have its js bindings done here
       $(this).children(':first').unwrap();
+      $('.sidenav').sidenav();
+      $('.acronym').hover(
+          function () { $('.nameFull').removeClass('hiddenAcronym'); },
+          function () { $('.nameFull').addClass('hiddenAcronym'); }
+      );
     });
   });
 });
-
-$('.acronym').hover(
-    function () { $('.nameFull').removeClass('hiddenAcronym'); },
-    function () { $('.nameFull').addClass('hiddenAcronym'); }
-);
