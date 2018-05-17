628af41b14d030f3c28efcec766bb07908b72613
RandyTjang <randy.tjang@gmail.com>
2018-05-18 03:03:12 +1000

    Added seahorse branding

--- js/common.js
+++ js/common.js
@@ -9,16 +9,38 @@ $( document ).ready(function() {
       $('.sidenav').sidenav();
       $('.acronym').hover(
         function () {
-          if($(window).width() >= 720) {
+          if($(window).width() >= 840) {
             $('.nameFull').removeClass('hiddenAcronym');
           }
         },
         function () {
-          if($(window).width() >= 720) {
+          if($(window).width() >= 840) {
             $('.nameFull').addClass('hiddenAcronym');
           }
         }
       );
+      // Replaces all <img source=".svg"></img> into inline svg
+      $('img[src$=".svg"]').each(function() {
+          var $img = jQuery(this);
+          var imgURL = $img.attr('src');
+          var attributes = $img.prop("attributes");
+
+          $.get(imgURL, function(data) {
+              // Get the SVG tag, ignore the rest
+              var $svg = jQuery(data).find('svg');
+
+              // Remove any invalid XML tags
+              $svg = $svg.removeAttr('xmlns:a');
+
+              // Loop through IMG attributes and apply on SVG
+              $.each(attributes, function() {
+                  $svg.attr(this.name, this.value);
+              });
+
+              // Replace IMG with SVG
+              $img.replaceWith($svg);
+          }, 'xml');
+      });
     });
   });
 });
