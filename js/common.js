a0335f0b214b27216bc84d610128bfca528d48e0
RandyTjang <randy.tjang@gmail.com>
2018-05-18 01:01:06 +1000

    Tried fixing header again

--- js/common.js
+++ js/common.js
@@ -10,7 +10,9 @@ $(function(){
   var includes = $('[data-include]');
   jQuery.each(includes, function(){
     var file = 'views/' + $(this).data('include') + '.html';
-    $(this).load(file);
+    $(this).load(file, function() {
+      $(this).children(':first').unwrap();
+    });
   });
 });
 
