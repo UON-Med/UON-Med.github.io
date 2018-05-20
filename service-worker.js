f201712a10b869a11cd7a9db648b9c5a4c167752
RandyTjang <randy.tjang@gmail.com>
2018-05-21 03:22:59 +1000

    Split outreach to own tab

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const buildNumber = '0.2.4.1b'
+const buildNumber = '0.2.4.1c'
 
 var dataCacheName = 'JMPdata-v' + buildNumber;
 var cacheName = 'JMPpwa-v' + buildNumber;
