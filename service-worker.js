d58260d2f55f685363ce4c626c80c26c1c6f40cf
RandyTjang <randy.tjang@gmail.com>
2018-05-20 15:35:40 +1000

    Made animation speeds more consistent

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const buildNumber = '0.2.3.9c'
+const buildNumber = '0.2.3.9d'
 
 var dataCacheName = 'JMPdata-v' + buildNumber;
 var cacheName = 'JMPpwa-v' + buildNumber;
