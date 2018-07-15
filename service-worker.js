8d58d157e2aee3b56d8c2e008cbc7b2088341258
RandyTjang <randy.tjang@gmail.com>
2018-07-16 04:16:39 +1000

    Updated with more calendars [113/136]

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const buildNumber = '0.2.4.9p'
+const buildNumber = '0.2.4.9q'
 
 var dataCacheName = 'JMPdata-v' + buildNumber;
 var cacheName = 'JMPpwa-v' + buildNumber;
