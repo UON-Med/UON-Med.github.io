94cfc25911a39634efe4065ccefb606cd2e64097
RandyTjang <randy.tjang@gmail.com>
2018-07-15 19:03:36 +1000

    Changed expected calendar completion date to Monday

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-const buildDate = '07:01pm, 15 Jul 2018';
+const buildDate = '07:03pm, 15 Jul 2018';
 
 const tooSmallForJMP = 850;
 const atTopOfPage = 100;
--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const buildNumber = '0.2.4.9i'
+const buildNumber = '0.2.4.9j'
 
 var dataCacheName = 'JMPdata-v' + buildNumber;
 var cacheName = 'JMPpwa-v' + buildNumber;
